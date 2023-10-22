08c0b1832c3b306081b54da57a178643cbf07798
creyes25 <carolina.reyes2022@gmail.com>
2023-10-21 21:27:28 -0500

    select elements that will be manipulated in the DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const input1 = document.querySelector('.num-1')
+const input2 = document.querySelector('.num-2')
+const addition = document.querySelector('.add')
+const minus = document.querySelector('.minus')
+const division = document.querySelector('.divide')
+const multiply = document.querySelector('.multiply')
+
